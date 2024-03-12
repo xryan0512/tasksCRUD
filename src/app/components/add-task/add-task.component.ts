8e89586b78c151c601752113e1e43e0d55670c9d
bryan <bryandev12@gmail.com>
2024-03-12 09:48:25 -0400

    add addBtn component

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
-  styleUrls: ['./add-task.component.css']
+  styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent {
-
+  constructor() {}
 }
