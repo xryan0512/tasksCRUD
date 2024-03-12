51da06367ade22142762ed95368903325aae394c
bryan <bryandev12@gmail.com>
2024-03-12 09:41:56 -0400

    Add create taks component and add sucess msg

---
+++ src/app/pages/create-task/create-task.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
+import { Task } from 'src/interfaces/task.interface';
+
+@Component({
+  selector: 'app-create-task',
+  templateUrl: './create-task.component.html',
+  styleUrls: ['./create-task.component.css'],
+})
+export class CreateTaskComponent {
+  task: Task = { title: '', isCompleted: false };
+  successMsg: boolean | null = null;
+  constructor(private taskService: TaskService) {}
+
+  addTask(): void {
+    this.taskService.createTaskItem(this.task).subscribe((res) => {
+      this.successMsg = true;
+      setTimeout(() => {
+        this.successMsg = false;
+      }, 3000);
+    });
+    this.task = { title: '', isCompleted: false };
+  }
+}
