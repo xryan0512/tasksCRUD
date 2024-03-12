d144e3254417b354e37f6324b4a8021055f7fa7c
bryan <bryandev12@gmail.com>
2024-03-12 09:38:30 -0400

    add Routes Create, detail, update Tasks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { CreateTaskComponent } from './pages/create-task/create-task.component';
+import { DetailTaskComponent } from './pages/detail-task/detail-task.component';
+import { UpdateTaskComponent } from './pages/update-task/update-task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TaskListComponent },
+  { path: 'createtask', component: CreateTaskComponent },
+  { path: 'detail/:id', component: DetailTaskComponent },
+  { path: 'update/:id', component: UpdateTaskComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
